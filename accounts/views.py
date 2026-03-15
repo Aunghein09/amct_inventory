673b9723dd8f795075cf27ee7414ed14801118a0
Aung Hein <aunghein09@gmail.com>
2026-03-16 01:31:55 +1100

    Remember login tab choice in session for redirect

--- accounts/views.py
+++ accounts/views.py
@@ -11,10 +11,17 @@ class CustomLoginView(auth_views.LoginView):
             next_url = request.GET.get("next")
             if next_url:
                 return redirect(next_url)
-            if request.user.is_staff:
-                return redirect("/admin/")
-            return redirect("/")
+            # Redirect based on which tab they originally logged in from
+            login_dest = request.session.get("login_dest", "/")
+            return redirect(login_dest)
         return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        # Store the intended destination in the session
+        next_url = self.request.POST.get("next", "/")
+        response = super().form_valid(form)
+        self.request.session["login_dest"] = next_url
+        return response
 
 
 @login_required
