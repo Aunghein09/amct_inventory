81e16013ef8f4631f057372adf78d5c84ef575be
Aung Hein <aunghein09@gmail.com>
2026-03-16 01:20:24 +1100

    Redirect authenticated users from login page (respects next param)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("redirect/", views.post_login_redirect, name="post_login_redirect"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
+
+
+class CustomLoginView(auth_views.LoginView):
+    template_name = "accounts/login.html"
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            next_url = request.GET.get("next", "/")
+            return redirect(next_url)
+        return super().get(request, *args, **kwargs)
 
 
 @login_required
